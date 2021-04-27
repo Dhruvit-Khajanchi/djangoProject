6e57a7ea5e8d103547c7b70e2e54e46600c0d4f9
Dhruvit-Khajanchi <dhruvikh299@gmail.com>
2021-04-27 19:44:14 +0530

    render, html and new features added

--- introduction_app/urls.py
+++ introduction_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home,about,search
+from .views import home,about,greetings,greeting2
 
 urlpatterns = [
     path("home/", home, name="hello-home"),
     path("about/", about, name="about_page"),
-    path("search/", search,name="search")
+    path("greetings/<str:name>", greetings,name="greeting"),
+    path("greetings/", greeting2,name="greetings2")
 ]
--- introduction_app/views.py
+++ introduction_app/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h2>This is my First attempt: </h2>");
+    # return HttpResponse("<h2>This is my First attempt: </h2>");
+    return render(request, "introduction_app/home.html",
+                  { 'title':'Home'
+
+                  })
 
 def about(request):
-    return HttpResponse("<h1>About Page</h1>");
+    # return HttpResponse("<h1>About Page</h1>");
+    return render(request, "introduction_app/about.html",
+                  { 'title': 'About'
 
-def search(request):
-    return HttpResponse("<h5>Search files here</h5>");
+                  })
+
+def greetings(request,name):
+    # return HttpResponse("<h5>Hello Dhruvit</h5>");
+    return render(request, "introduction_app/greetings.html",
+                  {'username':name,
+                   'title':'Greetings'
+    })
+
+def greeting2(request):
+    return render(request,"introduction_app/greeting2.html",
+                  {'title':'Greeting'
+
+                  })
